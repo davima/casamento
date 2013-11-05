d28a0043f5670482a35abe3b4fea0b3f27e6261d
davi <davi.ma@gmail.com>
2013-11-04 22:46:38 -0300

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Casamento::Application.config.secret_token = '4f9a02709c1119e7f6fc264d59115af300b46cf7a0b3bcb6bed9da8ef639156bc6eaef76a0fa4f9efdeee9903757216823e3fece7dc8fbc74345c80c0b6466c3'
